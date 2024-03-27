39d8736ee3691591b7ed1c1af5948e43c9e90bef
Muhammad Oka <oka@muhammadoka.dev>
2024-03-27 16:34:16 +0700

    feat: add cli interpreter

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,11 @@ enum OPERATION {
     JumpIfNonZero(usize),
 }
 
-fn main() {
-    if args().len() != 2 {
-        println!("Usage: ./bfi <source_code.bf>");
-        exit(1);
-    }
-
-    let source_path = args().into_iter().nth(1).unwrap();
-    let source_file = match File::open(&source_path) {
-        Ok(f) => f,
-        Err(_) => {
-            eprintln!("Cannot open file {}", &source_path);
-            exit(1);
-        }
-    };
-
-    let mut buffer_reader = BufReader::new(source_file);
-    let mut source_data = String::new();
-    let _ = buffer_reader.read_to_string(&mut source_data);
-
+fn lexer(data: &String) -> Vec<OPERATION> {
     let mut bracket_stack: Vec<usize> = Vec::new();
     let mut operations: Vec<OPERATION> = Vec::new();
 
-    // Lexer
-    for (i, char) in source_data.chars().enumerate() {
+    for (i, char) in data.chars().enumerate() {
         match char {
             '-' => {
                 operations.push(OPERATION::Subtract);
@@ -78,18 +59,19 @@ fn main() {
         }
     }
 
-    // Interpreter
-    let mut cells: [Wrapping<u8>; 30000] = [Wrapping(0); 30000];
+    return operations;
+}
+
+fn interpret(operations: Vec<OPERATION>, cells: &mut [Wrapping<u8>; 30000], cp: &mut usize) {
     let mut ip = 0;
-    let mut cp = 0;
 
     while ip < operations.len() {
         match operations[ip] {
             OPERATION::Add => {
-                cells[cp] += 1;
+                cells[*cp] += 1;
             }
             OPERATION::Subtract => {
-                cells[cp] -= 1;
+                cells[*cp] -= 1;
             }
             OPERATION::Input => {
                 let mut input = String::new();
@@ -107,7 +89,7 @@ fn main() {
 
                 match input.trim().parse::<u8>() {
                     Ok(num) => {
-                        cells[cp] = Wrapping(num);
+                        cells[*cp] = Wrapping(num);
                     }
                     Err(_) => {
                         eprintln!("Cannot parse input as u8!");
@@ -116,25 +98,72 @@ fn main() {
                 }
             }
             OPERATION::Output => {
-                print!("{}", cells[cp].0 as char);
+                print!("{}", cells[*cp].0 as char);
             }
             OPERATION::PointerLeft => {
-                cp -= 1;
+                *cp -= 1;
             }
             OPERATION::PointerRight => {
-                cp += 1;
+                *cp += 1;
             }
             OPERATION::JumpIfZero(jump_addr) => {
-                if cells[cp].0 == 0 {
+                if cells[*cp].0 == 0 {
                     ip = jump_addr;
                 }
             }
             OPERATION::JumpIfNonZero(jump_addr) => {
-                if cells[cp].0 != 0 {
+                if cells[*cp].0 != 0 {
                     ip = jump_addr;
                 }
             }
         }
         ip += 1;
+    }
+}
+
+fn main() {
+    let mut source_data = String::new();
+    let mut cells: [Wrapping<u8>; 30000] = [Wrapping(0); 30000];
+    let mut cp: usize = 0;
+
+    match args().len() {
+        1 => loop {
+            let mut input = String::new();
+
+            print!(">> ");
+            let _ = stdout().flush();
+
+            match stdin().read_line(&mut input) {
+                Ok(_) => {}
+                Err(_) => {
+                    eprintln!("Cannot read input!");
+                    exit(1);
+                }
+            }
+
+            let operations = lexer(&input);
+            interpret(operations, &mut cells, &mut cp);
+            println!();
+        },
+        2 => {
+            let source_path = args().into_iter().nth(1).unwrap();
+            let source_file = match File::open(&source_path) {
+                Ok(f) => f,
+                Err(_) => {
+                    eprintln!("Cannot open file {}", &source_path);
+                    exit(1);
+                }
+            };
+
+            let mut buffer_reader = BufReader::new(source_file);
+            let _ = buffer_reader.read_to_string(&mut source_data);
+
+            let operations = lexer(&source_data);
+            interpret(operations, &mut cells, &mut cp);
+        }
+        _ => {
+            println!("Usage: ./bfi <source_code.bf>");
+            exit(1);
+        }
     }
 }
